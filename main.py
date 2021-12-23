a370701a98b41e8453f50014e23201a132a1b92a
Sajad Ebrahimi <sajad.ebrahimi1378@gmail.com>
2021-12-23 16:56:26 +0330

    Split functions into related modules

--- main.py
+++ main.py
@@ -1,42 +1,21 @@
-import openpyxl
-import random
-from fastapi import FastAPI
-import uvicorn
-
-app = FastAPI()
-
-
-@app.get("/")
-async def read_items(ticket_id: int):
-    return {"ticket_id": ticket_id}
-
-
-def run():
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-
-
-def parse_users(file_path):
-    """
-    Parse names from a file
-    """
-    wb = openpyxl.load_workbook(file_path)
-    ws = wb.active
-    users_list = []
-    for i in range(2, ws.max_row + 1):
-        users_list.append({
-            'first_name': ws.cell(row=i, column=1).value,
-            'last_name': ws.cell(row=i, column=2).value,
-            'phone_number': ws.cell(row=i, column=3).value,
-            'enter_code': random.randint(10000, 99999),
-        })
-
-    return users_list
-
+from config import *
+import sys
 
 if __name__ == '__main__':
-    # users = parse_users(INPUT_FILE_PATH)
-    #
-    # for user in users:
-    #     print(user)
+    command = sys.argv[1] if len(sys.argv) > 1 else 'help'
 
-    run()
+    if command == 'add_ticket_id':
+        agree = input('This will overwrite the file. Are you sure? (y/n) ')
+        if agree == 'y':
+            from excel_handler import add_ticket_id
+            add_ticket_id(INPUT_FILE_PATH)
+
+    elif command == 'run_webserver':
+        from webserver import run_server
+        run_server()
+
+    elif command == 'help':
+        print('Available commands:')
+        print('\tadd_ticket_id')
+        print('\trun_webserver')
+        print('\thelp')
