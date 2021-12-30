4ac9fc0a230f45310420ca847474867d30ca41ba
Sajad Ebrahimi <sajad.ebrahimi1378@gmail.com>
2021-12-30 15:29:00 +0330

    Return proper message when password is not provided

--- server/webserver.py
+++ server/webserver.py
@@ -2,6 +2,7 @@ from server.config import *
 from server.excel_handler import get_users_from_excel
 from fastapi import FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
+from typing import Optional
 import uvicorn
 
 app = FastAPI()
@@ -20,7 +21,7 @@ users = get_users_from_excel(OUTPUT_FILE_PATH)
 
 def has_permission(password: str):
     if password is None:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Password Not Found")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Password required")
     if password != ADMIN_PASSWORD:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect password")
     else:
@@ -28,7 +29,7 @@ def has_permission(password: str):
 
 
 @app.get("/ticket/all")
-async def read_all_items(password: str):
+async def read_all_items(password: Optional[str] = None):
     if has_permission(password):
         return users
 
@@ -42,7 +43,7 @@ async def read_items(ticket_id: int):
 
 
 @app.get("/reception/{ticket_id}")
-async def verify_ticket(ticket_id: int, password: str):
+async def verify_ticket(ticket_id: int, password: Optional[str] = None):
     if has_permission(password):
         for user in users:
             if user['ticket_id'] == ticket_id:
@@ -50,7 +51,7 @@ async def verify_ticket(ticket_id: int, password: str):
 
 
 @app.get("/camera")
-async def has_camera_permission(password: str):
+async def has_camera_permission(password: Optional[str] = None):
     if has_permission(password):
         return {'has_camera_perm': True}
 
